36e5794f02e1e6ef7ef88f89ccc5d8381fdcd338
Kamen Kirilov <kamentrg91@gmail.com>
2018-09-26 14:20:16 +0300

    format main.js file, remove unnecessery images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,50 @@
-(function($){
+(function ($) {
 
-      $(document).on('ready',function() {
+	$(document).on('ready', function () {
 
-      moreInfo();
+		moreInfo();
 
-            // Load More button
-            // more info toggle 3 more boxes
-            function moreInfo() {
-            $('.load-more').each(function(){
-                  var   btn = $(this),
-                        info = $(this).siblings('.more-info');
+		// Load More button
+		// more info toggle 3 more boxes
+		function moreInfo() {
+			$('.load-more').each(function () {
+				var btn = $(this),
+					info = $(this).siblings('.more-info');
 
-                  btn.on('click',function(){
-                  info.slideToggle('active');
+				btn.on('click', function () {
+					info.slideToggle('active');
 
-                  if ($(this).hasClass('active')) {
-                        $(this).removeClass('active').text('View Less');
-                        info.removeClass('flex');
-                  } else {
-                        $(this).addClass('active').text('View More');
-                        info.addClass('flex');
-                        btn.hide();
-                  }
-                  });
-            });
-            }
+					if ($(this).hasClass('active')) {
+						$(this).removeClass('active').text('View Less');
+						info.removeClass('flex');
+					} else {
+						$(this).addClass('active').text('View More');
+						info.addClass('flex');
+						btn.hide();
+					}
+				});
+			});
+		}
 
-            // scroll indicator
-            $(function() {
-            $('a[href*=#library]').on('click', function(e) {
-                  e.preventDefault();
-                  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-            });
-            });
+		// scroll indicator
+		$(function () {
+			$('a[href*=#library]').on('click', function (e) {
+				e.preventDefault();
+				$('html, body').animate({
+					scrollTop: $($(this).attr('href')).offset().top
+				}, 500, 'linear');
+			});
+		});
 
-            // toggle class on hamburgegr to overflow:hidden on body
-            $('.navbar-toggler').on('click',function(){
-                  $('body').toggleClass('hamburger-on');
-            });
+		// toggle class on hamburgegr to overflow:hidden on body
+		$('.navbar-toggler').on('click', function () {
+			$('body').toggleClass('hamburger-on');
+		});
 
-            // NOTE TO ME!
-            // rewrite the button custom to change the hamburger to X icon
+		// NOTE TO ME!
+		// rewrite the button custom to change the hamburger to X icon
 
-      });
-
-
+	});
 
 
 })(jQuery);
